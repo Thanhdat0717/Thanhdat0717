55d71824ccbcd136ecbb1c738ca0788af4f6bceb
Thành Đạt <datvip762004@gmail.com>
2024-03-24 21:32:49 +0700

    vi du sile 56 chuong 3

---
+++ Lab3/HinhHoc.java
@@ -0,0 +1,22 @@
+package Lab3;
+
+public class HinhHoc {
+    public final float PI = 3.14f;
+    public String ten;
+    public float chuvi;
+    public float dientich;
+    public float thetich;
+
+    public void xuatten() {
+        System.out.print( ten + "\n");
+    }
+    public void inchuvi(){
+        System.out.print("chuvi =" + chuvi + "\n");
+    }
+    public void indientich(){
+        System.out.print("dientich =" + dientich + "\n");
+    }
+    public void inthetich(){
+        System.out.print("thetich =" + thetich + "\n");
+    }
+}
---
+++ Main/MainClass.java
@@ -0,0 +1,42 @@
+package Main;
+import Lab3.HinhTron;
+import Lab3.HinhTru;
+import Lab3.HinhChuNhat;
+import Lab3.HinhVuong;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        HinhTron hto = new HinhTron();
+        hto.xuatten();
+        hto.nhapbankinh();
+        hto.tinhchuvi();
+        hto.tinhdientich();
+        hto.inchuvi();
+        hto.indientich();
+
+        HinhTru htu = new HinhTru();
+        htu.xuatten();
+        htu.nhapchieucao();
+        htu.tinhdientich();
+        htu.indientich();
+
+        HinhChuNhat hcn = new HinhChuNhat();
+        hcn.xuatten();
+        hcn.nhapchieudai();
+        hcn.nhapchieurong();
+        hcn.tinhchuvi();
+        hcn.tinhdientich();
+        hcn.inchuvi();
+        hcn.indientich();
+
+        HinhVuong hv = new HinhVuong();
+        hv.xuatten();
+        hv.nhapcanh();
+        hv.tinhchuvi();
+        hv.tinhdientich();
+        hv.inchuvi();
+        hv.indientich();
+    }
+    
+}
