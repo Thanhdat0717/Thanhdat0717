9fbdf2578a6009906bd505a080d58383d3cf4112
Thành Đạt <datvip762004@gmail.com>
2024-04-21 12:48:54 +0700

    slide 67-85 chuong3

---
+++ Lab3/Configs.java
@@ -0,0 +1,10 @@
+package Lab3;
+
+public class Configs {
+    public static final int Nhan_Vien_Sep=1;
+    public static final int Nhan_Vien_Linh=2;
+    public static final long Luong_Nhan_Vien_Full_Time_Sep=2000000;
+    public static final long Luong_Nhan_Vien_Full_Time_Linh=1000000;
+    public static final long Luong_Lam_Them_Moi_Ngay=800000;
+    public static final long Luong_Nhan_Vien_Part_Time_Moi_Gio=100000;   
+    }
--- Main/MainClass.java
+++ Main/MainClass.java
@@ -38,5 +38,4 @@ public class MainClass {
         hv.inchuvi();
         hv.indientich();
     }
-    
 }
